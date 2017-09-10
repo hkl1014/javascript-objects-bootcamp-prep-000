d415824d73e68ec7167c167029068f22faff9f88
hkl1014 <linghangking93@hotmail.com>
2017-09-10 23:05:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {name:"song"}
 function updatePlaylist(playlist, name, song){
   return Object.assign({},playlist,{[name]:song})
 }
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName
+}
